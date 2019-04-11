2a01c0b7c6108101edb70e2fc35d3b338129d025
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-04-11 17:11:45 -0500

    Add songs views (#8)
    * Add show to index view
    
    * Add all views for song

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -4,6 +4,39 @@ module Admin
       @songs = Song.all
     end
 
+    def show
+      @song = Song.find(params[:id])
+    end
+
+    def new
+      @song = Song.new
+    end
+
+    def create
+      @song = Song.new(song_params)
+      @song.save
+      redirect_to admin_song_path(@song), notice: "The song was successfully created"
+    end
+    
+    def edit
+      @song = Song.find(params[:id])
+    end
+
+    def update
+      @song = Song.find(params[:id])
+      if @song.update(song_params)
+        redirect_to admin_song_path(@song), notice: "The song was successfully updated"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      song = Song.find(params[:id])
+      song.destroy
+      redirect_to admin_songs_path, notice: "The song was successfully deleted"
+    end
+
     private
 
     def song_params
