3b7bc0dbc34f0af5af8be120d9d0627bc3969f1b
Frank Davis Condezo Fabian <davis.con.fab@gmail.com>
2019-04-26 11:44:50 -0500

    Feature/cover image to artists (#56)
    * Add styles
    
    * Add views for welcome pages
    
    * Add devise
    
    * Add styles navbar
    
    * Install active storage
    
    * Add cover image  to songs
    
    * Move private methods
    
    This commit close #55
    
    * Add cover image to artists
    
    * Remove duplicate line

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -11,23 +11,18 @@ module Admin
     def new
       @artist = Artist.new
     end
-        
+
     def edit
       @artist = Artist.find(params[:id])
     end
 
-    def destroy
-      artist = Artist.find(params[:id])
-      artist.destroy
-      redirect_to admin_artists_path, notice: "The artist was successfully deleted"
-    end
-
-    private
-
     def create
       @artist = Artist.new(artist_params)
-      @artist.save
-      redirect_to admin_artist_path(@artist), notice: "The artist was successfully created"
+      if @artist.save
+        redirect_to admin_artist_path(@artist), notice: "The artist was successfully created"
+      else
+        render :new
+      end
     end
 
     def update
@@ -39,11 +34,18 @@ module Admin
       end
     end  
 
+    def destroy
+      artist = Artist.find(params[:id])
+      artist.destroy
+      redirect_to admin_artists_path, notice: "The artist was successfully deleted"
+    end
 
+    private
     def artist_params
       params.require(:artist).permit(
-        :name, 
-        :age
+        :name,
+        :age,
+        :cover
       )
     end
   end
